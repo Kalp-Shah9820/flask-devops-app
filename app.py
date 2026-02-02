7096f19182895263e8af0f0ad7e03a9e80d6e5ec
Kalp-Shah9820 <shahkalp9820@gmail.com>
2026-02-02 16:38:05 +0530

    Configure Flask app for cloud deployment

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
